9adad9e8045bf3d04117d49778cfe5e55c24234a
Raddames <raddamestonui48@gmail.com>
2024-04-28 02:12:27 +0300

    added transactions fetch

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Transaction from "./Transaction";
 
 function TransactionsList() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8001/transactions')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok.');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTransactions(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +37,23 @@ function TransactionsList() {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render a list of <Transaction> components here */}
+
+        {transactions.map((transaction) => (
+          <tr key={transaction.id}>
+            <th>
+              <h3 className="ui center aligned header">{transaction.date}</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">{transaction.description}</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">{transaction.category}</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">{transaction.amount}</h3>
+            </th>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
